17d1ec5051b441f0bfd83c65149705b68e1cb809
Daniel <daniel.roman.terrazas@gmail.com>
2025-11-04 00:18:47 -0400

    suma

--- 01-helloworld/app/src/main/java/com/daniel/helloworld/MainActivity.java
+++ 01-helloworld/app/src/main/java/com/daniel/helloworld/MainActivity.java
@@ -1,6 +1,10 @@
 package com.daniel.helloworld;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +14,10 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button button;
+    private EditText text1;
+    private EditText text2;
+    private TextView txt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +27,26 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        button = findViewById(R.id.button);
+        text1  = findViewById(R.id.num1);
+        text2  = findViewById(R.id.num2);
+        txt    = findViewById(R.id.resultado);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    String  s1 =  text1.getText().toString();
+                    String  s2 =  text2.getText().toString();
+                    int a = Integer.parseInt(s1);
+                    int b = Integer.parseInt(s2);
+                    String s="Resultado :"+Integer.toString(a+b);
+                    txt.setText(s);
+                }catch (Exception e){
+
+                }
+
+            }
         });
     }
 }
